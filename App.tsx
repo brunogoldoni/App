f2194e53cf4f5f69c8729ced6b710336fbab4074
Bruno Goldoni <bruno.goldoni@compasso.com.br>
2023-04-12 15:43:14 -0300

    StatusBar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { ActivityIndicator } from 'react-native';
+import { StatusBar, ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold, Roboto_900Black } from '@expo-google-fonts/roboto';
@@ -9,12 +9,19 @@ import { Routes } from './src/routes';
 
 import { WrapperActivityIndicator } from '@screens/styles'
 
-export default function App() {
+const App = () => {
  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold, Roboto_900Black});
 
   return (
-  <ThemeProvider theme={theme}>
-    {fontsLoaded ? <Routes /> : <WrapperActivityIndicator><ActivityIndicator /></WrapperActivityIndicator>}
-  </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        translucent
+        barStyle="dark-content"
+        backgroundColor="transparent"
+      />
+      {fontsLoaded ? <Routes /> : <WrapperActivityIndicator><ActivityIndicator /></WrapperActivityIndicator>}
+    </ThemeProvider>
   );
 }
+
+export default App;
