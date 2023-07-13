9fd84ed46470fbde2466fb8560ece5737fe2d183
Bruno de Mendonça Goldoni <bruno.goldoni@compasso.com.br>
2023-07-13 12:26:13 -0300

    Refactor - global styles

--- src/screens/styles.ts
+++ src/screens/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components/native";
 
 export const Container = styled(SafeAreaView)`
   flex: 1;
-  padding: 24px;
+  padding: 16px;
   background-color: ${({ theme }) => theme.COLORS.BACK_GROUND};
 `;
 
