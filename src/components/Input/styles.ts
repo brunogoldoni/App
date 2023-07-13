6e006538bbb3a89a694825bb324f31eb029d2c98
Bruno de Mendonça Goldoni <bruno.goldoni@compasso.com.br>
2023-07-13 12:03:02 -0300

    Input Component - Fix styles
    -remove flex to fix responsive layout;
    -change margin-bottom

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -2,12 +2,12 @@ import { TextInput } from "react-native";
 import styled from "styled-components/native";
 
 export const Container = styled(TextInput)`
-  flex: 1;
   width: 100%;
   padding: 16px;
   min-height: 56px;
   max-height: 56px;
   border-radius: 8px;
+  margin-bottom: 16px;
   text-transform: lowercase;
   color: ${({ theme }) => theme.COLORS.WHITE};
   font-size: ${({ theme }) => theme.FONT_SIZE.S_10}px;
