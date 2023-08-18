0c9a410fbb2b83c17d12c2399012e11f4fbb9b55
Bruno de Mendonça Goldoni <bruno.goldoni@compasso.com.br>
2023-08-18 15:08:52 -0300

    add relative path to theme

--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,7 @@ module.exports = function (api) {
             '@screens': './src/screens',
             '@storage': './src/storage',
             '@utils': './src/utils',
+            '@theme': './src/theme'
           },
         },
       ],
